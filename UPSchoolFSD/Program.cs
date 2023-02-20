8ce3b2120d5526158caa8d2621f98822c2026bd8
ik3 <aybikeboran7140@gmail.com>
2023-02-21 02:28:39 +0300

    Add project files.

---
+++ UPSchoolFSD/Program.cs
@@ -0,0 +1,17 @@
+﻿using LuckySister.Console;
+using UpSchool.Domain.Entities;
+
+SelectionManager selectionManager= new SelectionManager();
+
+selectionManager.AddAttendee("Aybike Boran");
+selectionManager.AddAttendee("Umay Boran");
+selectionManager.AddAttendee("Selcen Boran");
+selectionManager.AddAttendee("Ömer Faruk Sönmez");
+selectionManager.AddAttendee("Mayato Boran");
+
+int luckyCount = 3;
+selectionManager.MakeSelection(luckyCount);
+
+List<Attendee> luckyList = selectionManager.GetLuckyList();
+luckyList.ForEach(lucky => Console.WriteLine(lucky.FullName));
+Console.ReadLine();
---
+++ UPSchoolFSD/SelectionManager.cs
@@ -0,0 +1,69 @@
+﻿using UpSchool.Domain.Entities;
+
+namespace LuckySister.Console
+{
+    public class SelectionManager
+    {
+        private List<Attendee> Attendees { get; set; }
+        private List<Attendee> SelectedAttendees { get; set; }
+        private Random _random;
+        public SelectionManager(){
+            Attendees= new List<Attendee>();
+            SelectedAttendees= new List<Attendee>();
+            _random= new Random();
+            }
+        public SelectionManager(List<Attendee> initialAttendees)
+        {
+            Attendees = new List<Attendee>();
+
+            Attendees.AddRange(initialAttendees);
+
+            SelectedAttendees = new List<Attendee>();
+
+            _random = new Random();
+        }
+
+        public void AddAttendee(Attendee attendee)
+        {
+            Attendees.Add(attendee);
+        }
+        public void AddAttendee(string fullName)
+        {
+            Attendee attendee = new Attendee()
+            {
+                Id = Guid.NewGuid(),
+                FullName = fullName,
+                CreatedOn = DateTimeOffset.Now
+            };
+
+            Attendees.Add(attendee);
+        }
+
+        public void MakeSelection(int selectedCount)
+        {
+            if (selectedCount > Attendees.Count)
+            {
+                throw new Exception("LuckyCount cannot be more then the attendees count.");
+            }
+
+            for (int i = 0; i < selectedCount; i++)
+            {
+                SelectedAttendees.Add(GetRandomAttendee());
+            }
+        }
+
+        public Attendee GetRandomAttendee() {
+            var randomIndex = _random.Next(Attendees.Count);
+            var attendee= Attendees[randomIndex];
+            return SelectedAttendees.Any(x => x.Id == attendee.Id) ? GetRandomAttendee() : attendee;
+
+        }
+
+        public List<Attendee> GetLuckyList()
+        {
+            return SelectedAttendees;
+        }
+
+
+    }
+}
